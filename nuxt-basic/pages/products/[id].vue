8d8dac1e26c6f4b3e8d0e745c9274e1285a35e5b
NatChotPai <145831360+NatChotPai@users.noreply.github.com>
2024-11-07 20:19:18 +0700

    commit2
    ติดตั้ง types/node
    Dynamic Routing
    ดึงข้อมูลจาก API (useFetch)

---
+++ nuxt-basic/pages/products/[id].vue
@@ -0,0 +1,19 @@
+<template>
+    <div>
+        <h1>รายละเอียดสินค้าชิ้นที่ {{ id }}</h1>
+    </div>
+</template>
+
+<script setup>
+const {id} = useRoute().params
+useHead(
+    {
+        title:"Product Detail Page Website",
+        meta:[
+            {name:"Description" , content:"รายละเอียดสินค้าชิ้นที่"}
+        ]
+    }
+)
+</script>
+
+<style scoped></style>
--- nuxt-basic/pages/products/index.vue
+++ nuxt-basic/pages/products/index.vue
@@ -1,10 +1,16 @@
 <template>
     <div>
         <h1>สินค้าทั้งหมด</h1>
+        <ul v-for="product in data.products" v-bind:key="product.products">
+            <li>{{ product.title }}</li>
+        </ul>
     </div>
 </template>
 
 <script setup>
+const { data: data } = await useFetch('https://dummyjson.com/products')
+console.log(data)
+
 useHead(
     {
         title:"Product Page Website",
